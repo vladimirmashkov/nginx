979d548c3ac96f93b84e05522a53f4772dd74497
Vladimir Mashkov <vladimir@mashkov.com>
2022-09-14 15:39:26 +0300

    Update debian_11.sh

--- debian/debian_11.sh
+++ debian/debian_11.sh
@@ -3,4 +3,7 @@ cp /etc/resolv.conf /etc/resolv.conf.bak
 echo "nameserver 8.8.8.8" >  /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 apt install -y nginx
+systemctl start nginx
+systemctl enable nginx
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 
