f1f1b16d6023ba2e4490823725aa5558065099c0
Vladimir Mashkov <vladimir@mashkov.com>
2022-09-16 11:50:25 +0300

    update centos_7

--- centos/centos_7.sh
+++ centos/centos_7.sh
@@ -2,7 +2,10 @@
 cp /etc/resolv.conf /etc/resolv.conf.bak
 echo "nameserver 8.8.8.8" >  /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+yum install -y epel-release
+yum update -y && yum upgrade -y && yum update -y
 yum install -y nginx
+yum update -y && yum upgrade -y && yum update -y
 systemctl start nginx
 systemctl enable nginx
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
